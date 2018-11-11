ff0fd4bc49a33d869233b72634a5a123b6422e25
Vinoth G <vin10catchme@gmail.com>
2018-11-12 00:27:12 +0530

    solve error

--- src/com/codex/solid/order/CardPaymentProcessor.java
+++ src/com/codex/solid/order/CardPaymentProcessor.java
@@ -3,9 +3,15 @@ package com.codex.solid.order;
 public class CardPaymentProcessor implements IPaymentProcessor {
 
 	@Override
-	public void pay() {
+	public void pay(IPaymentDetails details) {
 		// TODO Auto-generated method stub
 		
 	}
+//
+//	@Override
+//	public void pay() {
+//		// TODO Auto-generated method stub
+//		
+//	}
 
 }
