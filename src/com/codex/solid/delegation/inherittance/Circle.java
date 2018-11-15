15441964678714ebdd2955db5ea4bd4aef10f101
Vinoth G <vin10catchme@gmail.com>
2018-11-16 00:42:44 +0530

    Updated

---
+++ src/com/codex/solid/delegation/inherittance/Circle.java
@@ -0,0 +1,11 @@
+package com.codex.solid.delegation.inherittance;
+
+public class Circle extends Shape {
+
+	@Override
+	void draw(Graphics graphics) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
