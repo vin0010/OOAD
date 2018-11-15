b105598007bc92000904936e2cd1fd4dcc659c30
Vinoth G <vin10catchme@gmail.com>
2018-11-15 16:32:08 +0530

    Added organizers

---
+++ src/com/codex/solid/template/Organizers.java
@@ -0,0 +1,11 @@
+package com.codex.solid.template;
+
+public class Organizers {
+	
+	public static void main(String[] args) {
+		Team team = new Team("Mumbai indians");
+		Player player = new AwayPlayer("Pollard", Category.ALLROUNDER, Designation.PLAYER);
+		team.addPlayer(player);
+		player.pay();
+	}
+}
