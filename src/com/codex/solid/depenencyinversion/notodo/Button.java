ccededd6ec515e35a7a754ddfafd48e0af8b3340
Vinoth G <vin10catchme@gmail.com>
2018-11-18 12:43:54 +0530

    dependency inversion bad example

---
+++ src/com/codex/solid/depenencyinversion/notodo/Button.java
@@ -0,0 +1,4 @@
+package com.codex.solid.depenencyinversion.notodo;
+
+public class Button {
+}
---
+++ src/com/codex/solid/depenencyinversion/notodo/SomeObject.java
@@ -0,0 +1,7 @@
+package com.codex.solid.depenencyinversion.notodo;
+
+public class SomeObject {
+	public void pressed() {
+		System.out.println("Pressed");
+	}
+}
