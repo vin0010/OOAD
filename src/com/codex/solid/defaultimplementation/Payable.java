d61539ca9459864f70a034c5da0bd9d1f88ca918
Vinoth G <vin10catchme@gmail.com>
2018-11-18 18:55:59 +0530

    updated ball socket notation

--- src/com/codex/solid/defaultimplementation/Payable.java
+++ src/com/codex/solid/defaultimplementation/Payable.java
@@ -5,6 +5,8 @@ public interface Payable {
 
 	static class Implementation implements Payable{
 		public void pay() {
+			//For ball socket notation add
+			AccountsPayable.getInstance().paying(this);
 			System.out.println("Pay is called!");
 		}
 	}
